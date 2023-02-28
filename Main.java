f333939ca6d93cc9e8e72a0edc20a78f4b864a5a
Sevgi BAŞAR <106137039+Sevgibsr1@users.noreply.github.com>
2023-03-01 01:30:59 +0300

    Java 3.2

--- Main.java
+++ Main.java
@@ -1,31 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+    Product product = new Product ();
+    product.setName ("Laptop");
+    product.setId(1);
+    product.setDescription ("Asus Laptop");
+    product.setPrice (5000);
+    product.setStockAmount(3);
 
-//        CreditManager creditManager = new CreditManager();
-//        creditManager.calculate();
-//
-//         Customer customer = new Customer();
-//         customer.city = "Istanbul";
-//
-//
-//         CustomerManager customerManager = new CustomerManager(customer);
-//         customerManager.save(); customerManager.delete();
-//
-//         Company company=new Company(); company.taxnumber="10000";
-//         company.companyName="Arçelik"; company.id=100;
-//
-//         CustomerManager customerManager2=new CustomerManager(new Person());
-//
-//         Person person=new Person(); person.nationalIdentity="123456";
-//
-//         Customer c1=new Customer();
-//         Customer c2=new Person();
-//         Customer c3=new Company();
+    ProductManager productManager= new ProductManager();
+    productManager.Add(product);
 
-        CustomerManager customerManager = new CustomerManager(new Customer(), new TeacherCreditManager());
-        customerManager.giveCredit();
-
-        }
-
-        }
-
+        System.out.println(product.getKod());
+    }
+}
