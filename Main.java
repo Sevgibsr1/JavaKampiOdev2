2a2edda76f475582a51eefb2cf0236f6e434b404
Sevgi BAŞAR <106137039+Sevgibsr1@users.noreply.github.com>
2023-03-06 01:36:00 +0300

    Java 3.2 Çoklu İmplementasyonu Kavramak

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        CustomerManager customerManager= new CustomerManager(new OracleCustomerDal());
-        customerManager.add();
 
 
 
